713f691759388731c53d3ba9601d10652c1c7918
fatinlion <fatinlion56@gmail.com>
2026-01-29 10:57:25 +0600

    added some filee

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Calculiam-
-It's a calculator web
+# Calculator App
+
+Simple open-source calculator app built with HTML, CSS, JavaScript and wrapped using Android WebView.
+
+## Features
+- Dark / Light mode
+- Calculation history
+- Offline support
+- No ads
+- No tracking
+
+## License
+MIT
